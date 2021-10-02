50483c69a59db453cbc91855e32cb96af3dc9aad
Pawan Benjamin <pawanbenjamin@gmail.com>
2021-10-01 20:09:28 -0400

    test 2

--- src/server/db/seedData.js
+++ src/server/db/seedData.js
@@ -1,28 +1,18 @@
-const client = require('./client');
-
+const client = require("./client");
 
 // Drop Tables
 
-
-
 // Create Tables
 
-
-
 // Create Initial Users
-
-
+const createUsers = () => {
+  console.log("test 2");
+};
 
 // Create Initial Products
 
-
-
 // Create Initial Orders
 
-
-
 // Create Rebuild Function which connects to clients runs each of the above functions
-
-
 
 // Export the rebuild function to use in the seed.js file
