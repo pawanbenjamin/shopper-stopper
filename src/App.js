2d2d36d4bc8d6d7dae57c44d9d7c39e9622905be
Pawan Benjamin <pawanbenjamin@gmail.com>
2021-10-01 20:25:10 -0400

    testing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Comp1 from "./components/Comp1";
+import Comp4 from "./components/Comp4";
 
 function App(props) {
   return (
     <div>
       <Comp1 />
+      <Comp4 />
     </div>
   );
 }
---
+++ src/components/Comp4.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Comp4(props) {
+  return (
+    <div>
+      <h1>OH ANOTHER ONE</h1>
+    </div>
+  );
+}
+
+export default Comp4;
